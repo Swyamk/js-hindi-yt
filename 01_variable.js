ab3589a362120ce561f3e64dfd5913b71e374762
Swyamk <143319984+Swyamk@users.noreply.github.com>
2023-12-10 19:09:12 +0000

    new 01

---
+++ 01_variable.js
@@ -0,0 +1,17 @@
+//console.log("swyam");
+const accountId = 144234
+let accountEmail = "swyam@google.com"
+var accountPassword = "12344"
+accountCity= "jaipur"
+
+// accountId = 2
+
+accountEmail= "wbjc@gmail.com"
+accountPassword = "123456"
+accountCity = "vskp"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail,accountPassword,accountCity]);
+
+// use const and let ; not var because of issue in block scope and functional scope
